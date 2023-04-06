b1a5131739b711090b23a155122e5b62bb2c25db
ADDREVALGUSTIANDI <127173045+ADDREVALGUSTIANDI@users.noreply.github.com>
2023-04-06 11:45:18 +0700

    latihan

---
+++ PBO_2211082001/src/addreval_070323/hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package addreval_070323;
+
+/**
+ *
+ * @author addreval
+ */
+public class hello {
+        public static void main(String [] args){
+        System.out.println("Hello World");
+        }
+    }
+
